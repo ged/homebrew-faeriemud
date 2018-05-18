ca6f6093d57cfcffc18bce122b7c66f466340a0b
Michael Granger <ged@FaerieMUD.org>
2018-05-17 21:52:29 -0700

    Update newton dynamics formula

--- Formula/newton-dynamics.rb
+++ Formula/newton-dynamics.rb
@@ -2,10 +2,10 @@ class NewtonDynamics < Formula
 	desc "A cross-platform life-like physics simulation library"
 	homepage "http://newtondynamics.com/"
 	url "https://github.com/MADEAPPS/newton-dynamics/archive/newton-3.14.tar.gz"
-	sha256 ""
+	sha256 "ccbd13512fdb4ac6adf2a12cb035dd866a2ec93f6fc35feba58531ad883dbdeb"
 
-	# Configure uses cmake internally
 	depends_on "cmake" => :build
+	depends_on "tinyxml"
 
 	def install
 		ENV.deparallelize
