88c4c709dde053b79bb6fb05c665134753883e1f
Michael Granger <ged@FaerieMUD.org>
2019-02-24 12:30:29 -0800

    Add overrides for czmq and zeromq

---
+++ Formula/czmq.rb
@@ -0,0 +1,74 @@
+class Czmq < Formula
+  desc "High-level C binding for ZeroMQ"
+  homepage "http://czmq.zeromq.org/"
+  url "https://github.com/zeromq/czmq/releases/download/v4.2.0/czmq-4.2.0.tar.gz"
+  sha256 "cfab29c2b3cc8a845749758a51e1dd5f5160c1ef57e2a41ea96e4c2dcc8feceb"
+
+  bottle do
+    cellar :any
+    sha256 "38d2b6120f6d06c9a45c895f52949a2ddd01f72d7e91d3ff83cd39c954492300" => :mojave
+    sha256 "1e414d17fd6c0a4dd9939e84091b5073c23d2477569d12b0ee08d6a425abea14" => :high_sierra
+    sha256 "c0b2b82ae2edfa4dc97f48789ed87050dc0fb602e85a2b510fee6336afe17a5c" => :sierra
+    sha256 "d6966061fd61f2440713473c4f65bb9fd541be2f3be78e1d3f56ca54d366202e" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/zeromq/czmq.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  option "with-drafts", "Build and install draft classes and methods"
+
+  depends_on "asciidoc" => :build
+  depends_on "pkg-config" => :build
+  depends_on "xmlto" => :build
+  depends_on "zeromq"
+
+  def install
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
+    args << "--enable-drafts" if build.with? "drafts"
+
+    system "./autogen.sh" if build.head?
+    system "./configure", *args
+    system "make"
+    system "make", "ZSYS_INTERFACE=lo0", "check-verbose"
+    system "make", "install"
+    rm Dir["#{bin}/*.gsl"]
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <czmq.h>
+
+      int main(void)
+      {
+        zsock_t *push = zsock_new_push("inproc://hello-world");
+        zsock_t *pull = zsock_new_pull("inproc://hello-world");
+
+        zstr_send(push, "Hello, World!");
+        char *string = zstr_recv(pull);
+        puts(string);
+        zstr_free(&string);
+
+        zsock_destroy(&pull);
+        zsock_destroy(&push);
+
+        return 0;
+      }
+    EOS
+
+    flags = ENV.cflags.to_s.split + %W[
+      -I#{include}
+      -L#{lib}
+      -lczmq
+    ]
+    system ENV.cc, "-o", "test", "test.c", *flags
+    assert_equal "Hello, World!\n", shell_output("./test")
+  end
+end
+
---
+++ Formula/zeromq.rb
@@ -0,0 +1,64 @@
+class Zeromq < Formula
+  desc "High-performance, asynchronous messaging library"
+  homepage "http://www.zeromq.org/"
+  url "https://github.com/zeromq/libzmq/releases/download/v4.3.1/zeromq-4.3.1.tar.gz"
+  sha256 "bcbabe1e2c7d0eec4ed612e10b94b112dd5f06fcefa994a0c79a45d835cd21eb"
+  revision 1
+
+  bottle do
+    cellar :any
+    sha256 "f5837a7056c827b6fbe3b7758f87d78969ff01e5f91ece40050d58a2762ccca5" => :mojave
+    sha256 "c520b34c98300a0b591559376b841050bc4f9d011392d8cebeb02f670de47fc0" => :high_sierra
+    sha256 "7fbd2a2be3dcf6e83760627d0e1327dacebb9b39359d729438dd2468fe3b89e0" => :sierra
+  end
+
+  head do
+    url "https://github.com/zeromq/libzmq.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "asciidoc" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "xmlto" => :build
+
+  depends_on "libsodium"
+
+  def install
+    # Work around "error: no member named 'signbit' in the global namespace"
+    if MacOS.version == :high_sierra
+      ENV.delete("HOMEBREW_SDKROOT")
+      ENV.delete("SDKROOT")
+    end
+
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    # Disable libunwind support due to pkg-config problem
+    # https://github.com/Homebrew/homebrew-core/pull/35940#issuecomment-454177261
+
+    system "./autogen.sh" if build.head?
+    system "./configure", "--enable-drafts", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <assert.h>
+      #include <zmq.h>
+
+      int main()
+      {
+        zmq_msg_t query;
+        assert(0 == zmq_msg_init_size(&query, 1));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lzmq", "-o", "test"
+    system "./test"
+    system "pkg-config", "libzmq", "--cflags"
+    system "pkg-config", "libzmq", "--libs"
+  end
+end
